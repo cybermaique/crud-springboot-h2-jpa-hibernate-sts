91ff3942ebaac99733ff9683b8eeecabf334e1e0
Maique Douglas Moraes da Silva <maique.silva@digitro.com>
2021-12-05 14:16:09 -0300

    user entity and resource

---
+++ src/main/java/com/projectcrud/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.projectcrud.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectcrud.entities.User;
+
+@RestController //para definir como aplicação web q é implementado por  rest controller.
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping //pra indicar q o método vai responder requisição do tipo get do http
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maique", "maiqued.18@gmail.com", "51983518820", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
